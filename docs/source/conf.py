15aa5df9b002940ca24467edccf352b78a8ee32a
stuti <up2199677@myport.ac.uk>
2026-04-25 00:25:28 +0100

    Refactor conf.py to remove Django setup and streamline mock module handling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,39 +1,24 @@
 import os
 import sys
+from unittest.mock import MagicMock
 
 BASE_DIR = r"C:\Users\stuti\OneDrive\SETAP\SETAP CW\TERM 2 CW\UNIsoc"
 sys.path.insert(0, BASE_DIR)
 
-import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.config.settings')
-django.setup()
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
 
-# -- Project information --
-project = 'UNIsoc'
-author = 'Your Team'
-release = '0.1'
-version = '0.1.0'
-
-# -- General configuration --
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',    
-    'sphinx.ext.viewcode',     
-    'sphinx.ext.duration',
+MOCK_MODULES = [
+    'django', 'django.db', 'django.db.models', 'django.utils',
+    'django.utils.timezone', 'django.core', 'django.core.mail',
+    'django.contrib', 'django.contrib.auth', 'django.contrib.auth.models',
+    'django.contrib.auth.base_user', 'django.core.validators', 'django.conf',
+    'rest_framework', 'rest_framework.views', 'rest_framework.response',
+    'rest_framework.permissions', 'rest_framework.exceptions',
+    'rest_framework.generics', 'flask',
+    'celery', 'config.celery'
 ]
 
-autosummary_generate = True
-
-# Better autodoc output
-autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'show-inheritance': True,
-}
-
-# Templates
-templates_path = ['_templates']
-
-# Theme
-html_theme = 'sphinx_rtd_theme'
+sys.modules.update((mod, Mock()) for mod in MOCK_MODULES)
