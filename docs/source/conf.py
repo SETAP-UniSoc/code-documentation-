6bc7e4745fc4958aa5ad0685b030679dbeb6d522
stuti <up2199677@myport.ac.uk>
2026-04-25 00:33:21 +0100

    Refactor conf.py to streamline mock module handling and ensure correct backend path for Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,24 +1,27 @@
 import os
 import sys
-from unittest.mock import MagicMock
 
 BASE_DIR = r"C:\Users\stuti\OneDrive\SETAP\SETAP CW\TERM 2 CW\UNIsoc"
-sys.path.insert(0, BASE_DIR)
 
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
+# Point to backend so Sphinx can import your project
+sys.path.insert(0, os.path.join(BASE_DIR, "backend"))
 
-MOCK_MODULES = [
-    'django', 'django.db', 'django.db.models', 'django.utils',
-    'django.utils.timezone', 'django.core', 'django.core.mail',
-    'django.contrib', 'django.contrib.auth', 'django.contrib.auth.models',
-    'django.contrib.auth.base_user', 'django.core.validators', 'django.conf',
-    'rest_framework', 'rest_framework.views', 'rest_framework.response',
-    'rest_framework.permissions', 'rest_framework.exceptions',
-    'rest_framework.generics', 'flask',
-    'celery', 'config.celery'
+# ---- Sphinx extensions (IMPORTANT) ----
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
 ]
 
-sys.modules.update((mod, Mock()) for mod in MOCK_MODULES)
+autosummary_generate = True
+
+autodoc_mock_imports = [
+    "django",
+    "django.db",
+    "django.utils",
+    "django.core",
+    "django.contrib",
+    "rest_framework",
+    "flask",
+    "celery",
+    "config.celery",
+]
