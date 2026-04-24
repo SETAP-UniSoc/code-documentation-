9f6f921b16ef02652b49b8c7e814873fba068e93
stuti <up2199677@myport.ac.uk>
2026-04-24 23:58:54 +0100

    Fix BASE_DIR path in conf.py for correct project root resolution

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../../'))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../../../'))
 sys.path.insert(0, BASE_DIR)
 
 import django
